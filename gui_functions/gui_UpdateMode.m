5a26f65d2eb834ffc4c0671070b0048ac8c9c0f5
Ben Frazier <frazibw1@umd.edu>
2018-10-11 15:07:36 -0400

    moving code around

--- gui_functions/gui_UpdateMode.m
+++
@@ -1,6 +0,0 @@
-function handles = gui_UpdateMode(mode,handles)
-
-handles.mode = mode;
-setfield(handles.guiTimer,'UserData', mode);
-gui_updateStatusMessage(handles,'');
-guidata(handles.hfig,handles);
--- gui_functions/gui_updateStatusMessage.m
+++
@@ -1,4 +0,0 @@
-function gui_updateStatusMessage(handles,message)
-
-message = sprintf('%s ... %s',handles.mode,message);
-set(handles.statusBar,'Text',message);
