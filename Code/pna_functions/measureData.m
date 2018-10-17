733eaa7a36d3e3035c445e7ca6fdcef5760236f5
Ben Frazier <frazibw1@umd.edu>
2018-10-17 16:17:32 -0400

    code clean up, making sure time transform limits are in range

--- Code/pna_functions/measureData.m
+++ Code/pna_functions/measureData.m
@@ -19,9 +19,10 @@ if (eCal == true)
 end
 
 %% Compute and Capture S parameter Measurements
-fprintf(obj1, 'SYSTem:PRESet'); % preset to factory defined default settings
-ready = query(obj1, '*OPC?'); % Check if preset is complete
+%fprintf(obj1, 'SYSTem:PRESet'); % preset to factory defined default settings
+%ready = query(obj1, '*OPC?'); % Check if preset is complete
 fprintf(obj1, ['SENS:SWE:POINTS ', num2str(NOP)]); % set number of points
+
 fprintf(obj1, 'OUTP ON'); % turn RF power on
 fprintf(obj1, 'SENS1:AVER OFF'); % turn averaging off
 fprintf(obj1, 'SENS:SWE:MODE HOLD'); % set sweep mode to hold
@@ -44,6 +45,9 @@ fprintf(obj1, 'FORM REAL,64');
 fprintf(obj1, 'MMEM:STOR:TRAC:FORM:SNP RI');
 fprintf(obj1, 'INIT:IMM'); % send trigger to initiate one sweep
 fprintf(obj1, '*WAI'); % wait until sweep is complete
+fprintf(obj1, 'SENS:FREQ:STAR 8E9'); %set the start frequency
+fprintf(obj1, 'SENS:FREQ:STOP 12E9'); %set the stop frequency
+
 for iter = 1:N
     %% Record one sweep
     %% S cav - Frequency Domain
@@ -54,25 +58,28 @@ for iter = 1:N
         disp(lstring)
     end
     fprintf(s1,['I',num2str(DIRECTION*64*12000/N),',1700,0,0,5000,5000,1000,0,1000,1000,50,64']); pause(1+1/1700*60*(12000/N)); % move the steppermotor counterclockwise by 1.8 degrees (1.8*4 was replaced by 360/N to make the code adjust ensemble as the user sets it.
-    fprintf(obj1, 'CALC:TRAN:TIME:STATE OFF'); % turn off transfrom (to time domain)
-    fprintf(obj1, 'INIT:IMM'); % send trigger to initiate one sweep
-    fprintf(obj1, '*WAI'); % wait until sweep is complete
+    
+    [Freq, SCf11, SCf12, SCf21, SCf22] = getSCavFrequencyDomain(obj1,NOP);
+    
+    %fprintf(obj1, 'CALC:TRAN:TIME:STATE OFF'); % turn off transfrom (to time domain)
+    %fprintf(obj1, 'INIT:IMM'); % send trigger to initiate one sweep
+    %fprintf(obj1, '*WAI'); % wait until sweep is complete
     % Request frequency domain S paramter measurement in real and imaginary
-    fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
-    fprintf(obj1, ['CALC:DATA:SNP? ',num2str(num_ports)]); % Ask for S parameter data
-    X = binblockread(obj1, 'float64');
-    fprintf(obj1, '*WAI'); % wait until data tranfer is complete
-    Freq = X(1:(NOP));
+    %fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
+    %fprintf(obj1, ['CALC:DATA:SNP? ',num2str(num_ports)]); % Ask for S parameter data
+    %X = binblockread(obj1, 'float64');
+    %fprintf(obj1, '*WAI'); % wait until data tranfer is complete
+    %Freq = X(1:(NOP));
     %Importing the two port data
-    S11R = X(NOP+1:NOP+(NOP));
-    S11I = X(2*NOP+1:2*NOP+(NOP));
-    S21R = X(3*NOP+1:3*NOP+(NOP));     S21I = X(4*NOP+1:4*NOP+(NOP));
-    S12R = X(5*NOP+1:5*NOP+(NOP));     S12I = X(6*NOP+1:6*NOP+(NOP));
-    S22R = X(7*NOP+1:7*NOP+(NOP));     S22I = X(8*NOP+1:8*NOP+(NOP));
-    SCf(:,1,iter) = S11R + 1i*S11I; %One port line
-    SCf(:,2,iter) = S21R + 1i*S21I;
-    SCf(:,3,iter) = S12R + 1i*S12I;
-    SCf(:,4,iter) = S22R + 1i*S22I;
+    %S11R = X(NOP+1:NOP+(NOP));
+    %S11I = X(2*NOP+1:2*NOP+(NOP));
+    %S21R = X(3*NOP+1:3*NOP+(NOP));     S21I = X(4*NOP+1:4*NOP+(NOP));
+    %S12R = X(5*NOP+1:5*NOP+(NOP));     S12I = X(6*NOP+1:6*NOP+(NOP));
+    %S22R = X(7*NOP+1:7*NOP+(NOP));     S22I = X(8*NOP+1:8*NOP+(NOP));
+    SCf(:,1,iter) = SCf11; %One port line
+    SCf(:,2,iter) = SCf12;
+    SCf(:,3,iter) = SCf21;
+    SCf(:,4,iter) = SCf22;
     Time = toc;
 	
 	lstring = sprintf('Measuring Scav in frequency domain at position %d of %d, elapsed time = %0.3f', iter,N,Time);
@@ -84,31 +91,35 @@ for iter = 1:N
 	
     %% S rad - Frequency Domain
     for i = 1:10
-        fprintf(obj1, 'CALC:FILT:TIME:STATE ON'); % tunn on gating is on
-        start_time = -l*i/(3E8); stop_time = l*i/(3E8); %set the start and stop time for transforming to time domain
-        fprintf(obj1, ['CALC:FILT:TIME:START ', num2str(start_time)]);
-        fprintf(obj1, ['CALC:FILT:TIME:STOP ', num2str(stop_time)]);
-        fprintf(obj1, 'INIT:IMM'); % send trigger to initiate one sweep
-        fprintf(obj1, '*WAI'); % wait until sweep is complete
+    lstring = sprintf('Measuring Srad in frequency domain at gate %d of %d', i,10);
+    if (useGUI == true)
+        logMessage(handles.jEditbox,lstring);
+    else
+        disp(lstring)
+    end
+        [Freq, Srad11, Srad12, Srad21, Srad22] = getSRadFrequencyDomain(obj1,l,i,NOP);
+     %   fprintf(obj1, 'CALC:FILT:TIME:STATE ON'); % tunn on gating is on
+      %  start_time = -l*i/(3E8); stop_time = l*i/(3E8); %set the start and stop time for transforming to time domain
+       % fprintf(obj1, ['CALC:FILT:TIME:START ', num2str(start_time)]);
+        %fprintf(obj1, ['CALC:FILT:TIME:STOP ', num2str(stop_time)]);
+        %fprintf(obj1, 'INIT:IMM'); % send trigger to initiate one sweep
+        %fprintf(obj1, '*WAI'); % wait until sweep is complete
         % Request frequency domain S paramter measurement in real and imaginary
-        fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
-        fprintf(obj1, ['CALC:DATA:SNP? ',num2str(num_ports)]); % Ask for S parameter data
-        X = binblockread(obj1, 'float64');
-        fprintf(obj1, '*WAI'); % wait until data tranfer is complete
-        Freq = X(1:(NOP));
-        S11R = X(NOP+1:NOP+(NOP));
-        S11I = X(2*NOP+1:2*NOP+(NOP));
+        %fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
+        %fprintf(obj1, ['CALC:DATA:SNP? ',num2str(num_ports)]); % Ask for S parameter data
+        %X = binblockread(obj1, 'float64');
+        %fprintf(obj1, '*WAI'); % wait until data tranfer is complete
+        %Freq = X(1:(NOP));
+        %S11R = X(NOP+1:NOP+(NOP));
+        %S11I = X(2*NOP+1:2*NOP+(NOP));
         %2 Port Data Collection
-        S21R = X(3*NOP+1:3*NOP+(NOP));     S21I = X(4*NOP+1:4*NOP+(NOP));
-        S12R = X(5*NOP+1:5*NOP+(NOP));     S12I = X(6*NOP+1:6*NOP+(NOP));
-        S22R = X(7*NOP+1:7*NOP+(NOP));     S22I = X(8*NOP+1:8*NOP+(NOP));
-        Srad(:,2,iter) = S21R + 1i*S21I;
-        Srad(:,3,iter) = S12R + 1i*S12I;
-        Srad(:,4,iter) = S22R + 1i*S22I;
-        Srad(:,1,iter,i) = S11R + 1i*S11I; %2 Port Data Collected
-        Srad(:,2,iter,i) = S12R + 1i*S12I;
-        Srad(:,3,iter,i) = S21R + 1i*S21I;
-        Srad(:,4,iter,i) = S22R + 1i*S22I;
+        %S21R = X(3*NOP+1:3*NOP+(NOP));     S21I = X(4*NOP+1:4*NOP+(NOP));
+        %S12R = X(5*NOP+1:5*NOP+(NOP));     S12I = X(6*NOP+1:6*NOP+(NOP));
+        %S22R = X(7*NOP+1:7*NOP+(NOP));     S22I = X(8*NOP+1:8*NOP+(NOP));
+        Srad(:,1,iter,i) = Srad11; %2 Port Data Collected
+        Srad(:,2,iter,i) = Srad12;
+        Srad(:,3,iter,i) = Srad21;
+        Srad(:,4,iter,i) = Srad22;
         Time = toc;
     end
     lstring = sprintf('Measuring Srad in frequency domain at position %d of %d, elapsed time = %0.3f', iter,N,Time);
@@ -117,27 +128,33 @@ for iter = 1:N
     else
         disp(lstring)
     end
-	
+    
+    [t,SCt11, SCt12, SCt21, SCt22] = getSCavTimeDomain(obj1,NOP,handles); 
+    SCt(:,1,iter) = SCt11;
+    SCt(:,2,iter) = SCt12;
+    SCt(:,3,iter) = SCt21;
+    SCt(:,4,iter) = SCt22;
+    
     %% S cav - time domain
-    for port=1:4
-        fprintf(obj1,['CALC:PAR:SEL ', meas_name(port,:)]); %(Select the  measurement trace.
-        fprintf(obj1, 'CALC:TRAN:TIME:STATE ON'); % ensure tranform is on
-        fprintf(obj1, 'CALC:FILT:TIME:STATE OFF'); % ensure gating is off
-        start_time = -0.5E-6; stop_time = 10E-6; %set the start and stop time for transforming to time domain
-        fprintf(obj1, ['CALC:TRAN:TIME:START ', num2str(start_time)]);
-        fprintf(obj1, ['CALC:TRAN:TIME:STOP ', num2str(stop_time)]);
-        fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
-        fprintf(obj1, 'CALC:FORM REAL'); % Set format to get REAL data
-        fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
-        fprintf(obj1, 'CALC:DATA? FDATA'); % Request data
-        SR = binblockread(obj1, 'float64'); % Read data
-        fprintf(obj1, 'CALC:FORM IMAG'); % Set format to get IMAGINARY data
-        fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
-        fprintf(obj1, 'CALC:DATA? FDATA'); % Request data
-        SI = binblockread(obj1, 'float64'); % Read data
-        SCt(:,port,iter) = SR + 1i*SI; % Combine the real and imaginary parts of time domain
-        t = linspace(start_time,stop_time,length(SCt))';
-    end
+   % for port=1:4
+    %    fprintf(obj1,['CALC:PAR:SEL ', meas_name(port,:)]); %(Select the  measurement trace.
+     %   fprintf(obj1, 'CALC:TRAN:TIME:STATE ON'); % ensure tranform is on
+      %  fprintf(obj1, 'CALC:FILT:TIME:STATE OFF'); % ensure gating is off
+       % start_time = -0.5E-6; stop_time = 10E-6; %set the start and stop time for transforming to time domain
+        %fprintf(obj1, ['CALC:TRAN:TIME:START ', num2str(start_time)]);
+        %fprintf(obj1, ['CALC:TRAN:TIME:STOP ', num2str(stop_time)]);
+        %fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
+        %fprintf(obj1, 'CALC:FORM REAL'); % Set format to get REAL data
+        %fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
+        %fprintf(obj1, 'CALC:DATA? FDATA'); % Request data
+        %SR = binblockread(obj1, 'float64'); % Read data
+        %fprintf(obj1, 'CALC:FORM IMAG'); % Set format to get IMAGINARY data
+        %fprintf(obj1, 'DISP:WIND:Y:AUTO'); % Autoscale
+        %fprintf(obj1, 'CALC:DATA? FDATA'); % Request data
+        %SI = binblockread(obj1, 'float64'); % Read data
+        %SCt(:,port,iter) = SR + 1i*SI; % Combine the real and imaginary parts of time domain
+        %t = linspace(start_time,stop_time,length(SCt))';
+    %end
     Time = toc;
 	
 	lstring = sprintf('Measuring Scav in time domain at position %d of %d, elapsed time = %0.3f', iter,N,Time);
