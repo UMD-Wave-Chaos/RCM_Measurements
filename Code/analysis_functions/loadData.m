ded0146b3b044d85e2aefdfa8c942182761c49f0
Ben Frazier <frazibw1@umd.edu>
2018-10-19 14:52:13 -0400

    bug fix

--- Code/analysis_functions/loadData.m
+++ Code/analysis_functions/loadData.m
@@ -1,10 +1,10 @@
 function [t, SCt, Freq, SCf, Srad,V,l,N,NOP,nRCM] = loadData(filename)
 
-%log the data
+%load the data
 
-Srad = h5read(filename,'/Measurements/Srad');
-SCf = h5read(filename,'/Measurements/SCf');
-SCt = h5read(filename,'/Measurements/SCt');
+%Srad = h5read(filename,'/Measurements/Srad');
+%SCf = h5read(filename,'/Measurements/SCf');
+%SCt = h5read(filename,'/Measurements/SCt');
 t = h5read(filename,'/Measurements/t');
 Freq = h5read(filename,'/Measurements/Freq');
 N = h5readatt(filename,'/','Nrealizations');
@@ -14,14 +14,14 @@ l = h5readatt(filename,'/','l');
 nRCM = h5readatt(filename,'/','nRCM');
 
 
-% Srad_r = h5read(filename,'/Measurements/Srad_real');
-% SCf_r = h5read(filename,'/Measurements/SCf_real');
-% SCt_r = h5read(filename,'/Measurements/SCt_real');
+Srad_r = h5read(filename,'/Measurements/Srad_real');
+SCf_r = h5read(filename,'/Measurements/SCf_real');
+SCt_r = h5read(filename,'/Measurements/SCt_real');
 % 
-% Srad_i = h5read(filename,'/Measurements/Srad_imag');
-% SCf_i = h5read(filename,'/Measurements/SCf_imag');
-% SCt_i = h5read(filename,'/Measurements/SCt_imag');
+Srad_i = h5read(filename,'/Measurements/Srad_imag');
+SCf_i = h5read(filename,'/Measurements/SCf_imag');
+SCt_i = h5read(filename,'/Measurements/SCt_imag');
 % 
-% Srad = Srad_r + 1j*Srad_i;
-% SCf = SCf_r + 1j*SCf_i;
-% SCt = SCt_r + 1j*SCt_i;
+Srad = Srad_r + 1j*Srad_i;
+SCf = SCf_r + 1j*SCf_i;
+SCt = SCt_r + 1j*SCt_i;
