63c8d4ec0d1426dda2712fbfe0680d163d96d4e5
Ben Frazier <bwfrazier@gmail.com>
2018-10-25 15:08:55 -0400

    clean up

--- Code/pna_functions/measureData.m
+++ Code/pna_functions/measureData.m
@@ -1,5 +1,28 @@
 function [t, SCt, Freq, SCf, Srad] = measureData(obj1,s1, Settings,varargin)  
+%[t, SCt, Freq, SCf, Srad] = measureData(obj1, s1, Settings) 
+%[t, SCt, Freq, SCf, Srad] = measureData(obj1, s1, Settings, handles) 
+% This function handles measuring the wave chaotic S parameters for a
+% cavity
+% Inputs:
+% obj1 - connection to the PNA through the Matlab Instrumentation Toolbox,
+% this can be either a VISA-TCPIP object or a GPIB object
+% s1 - serial connection to the stepper motor
+% Settings - structure containing Settings read in from the config file
+% handles - optional input from the GUI 
+% 
+% Outputs:
+% t - Time vector from the PNA taken during the SCt measurement, will be of
+% size NOP x 1
+% SCt - Scav measurements in the time domain, will be of size NOP
+% x 4 x N
+% Freq - Time vector from the PNA taken during the Srad measurement, will be of
+% size NOP x 1
+% SCf - Scav measurements in the frequency domain, will be of size NOP x 4
+% x N
+% Srad - Srad measurements in the frequency domain (gated), will be of size
+% NOP x 4 x N x 10
 
+%% check to see if the handles was passed in to indicate GUI operation
 if (nargin == 4)
     useGUI = true;
     handles = varargin{1};
@@ -7,7 +30,7 @@ else
     useGUI = false;
 end
 
-%get the inputs from the Settings object
+%% get the inputs from the Settings object
 NOP = Settigns.NOP;
 N = Settings.N;
 eCal = Settings.electronicCalibration;
@@ -17,10 +40,10 @@ transformStart = Settings.transformStart;
 transformStop = Settings.transformStop;
 direction = Settings.direction;
 nStepsPerRevolution = Settings.nStepsPerRevolution;
+Ngates = 10;
 
-tic;
-
-%calibration step
+%% check to see if we need to perform the electric calibration
+% normally the PNA will be manually calibrated
 if (eCal == true)
     for k = 1:15
      cal_name = ['cal_for_',date,num2str(k)];
@@ -28,14 +51,25 @@ if (eCal == true)
     end
 end
 
-%% initialize the instruments
-
+%% initialize
+% initialize the instrumentation
 initializePNA(obj1,NOP,fStart,fStop);
 startPos = getStepperMotorPosition(s1);
 
+% initialize the variables 
+SCf = zeros(NOP,4,N);
+SCt = zeros(NOP,4,N);
+Srad = zeros(NOP,4,N,Ngates);
+
+% start the timer
+tic;
+
+
+%% Loop over the number of requested iterations
+% take measurements at each stepper motor position
+
 for iter = 1:N
-    %% Record one sweep
-    %% S cav - Frequency Domain
+    %% move the stepper motor
     stepDistance = direction*nStepsPerRevolution/N;
     waitTime = 10;
 	lstring = sprintf('Moving mode stirrer for position %d of %d: Moving %d steps and pausing %0.3f seconds',iter, N,stepDistance,waitTime);
@@ -48,11 +82,10 @@ for iter = 1:N
     newPos = getStepperMotorPosition(s1);
     
     if newPos - startPos ~= stepDistance
-        warnStr = sprintf('Stepped %d steps, expected %d steps',newPos-startPos,stepDistance);
-        warning(warnStr);
+        warning('Stepped %d steps, expected %d steps',newPos-startPos,stepDistance);
     end
 
-    %measure SCav in the frequency domain - this is an ungated measurement of the S parameters
+    %% measure SCav in the frequency domain - this is an ungated measurement of the S parameters
     [Freq, SCf11, SCf12, SCf21, SCf22] = getUngatedSParametersFrequencyDomain(obj1,NOP);
     SCf(:,1,iter) = SCf11; 
     SCf(:,2,iter) = SCf12;
@@ -67,8 +100,8 @@ for iter = 1:N
         disp(lstring)
     end
 	
-    %% S rad - Frequency Domain
-    for i = 1:10
+    %% Loop over 10 iterations of gating times and measure Srad in the frequency domain
+    for i = 1:Ngates
         lstring = sprintf('Measuring Srad in frequency domain at gate %d of %d', i,10);
         if (useGUI == true)
             logMessage(handles.jEditbox,lstring);
@@ -92,14 +125,13 @@ for iter = 1:N
         disp(lstring)
     end
 
-    %measure SCav in the time domain using the requested transform times
+    %% measure SCav in the time domain using the requested transform times
     [t,SCt11, SCt12, SCt21, SCt22] = getSParametersTimeDomain(obj1,NOP,transformStart,transformStop); 
     SCt(:,1,iter) = SCt11;
     SCt(:,2,iter) = SCt12;
     SCt(:,3,iter) = SCt21;
     SCt(:,4,iter) = SCt22;
     
-    %% S cav - time domain
     Time = toc;
 	lstring = sprintf('Measuring Scav in time domain at position %d of %d, elapsed time = %0.3f', iter,N,Time);
     if (useGUI == true)
@@ -108,6 +140,7 @@ for iter = 1:N
         disp(lstring)
     end
     
+    %% update elapsed time and predict time remaining to complete
     averagetime = Time/iter;
 	predictedTime = averagetime*(N-iter);
     lstring = sprintf('Measurement Step %d of %d Completed, Predicted remaining time = %s s',iter,N,num2str(predictedTime));
