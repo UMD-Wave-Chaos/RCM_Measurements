44dead55accceb43f4ac39b133a59cbe876d486b
Ben Frazier <bwfrazier@gmail.com>
2018-10-29 16:32:40 -0400

    updating distribution calculations

--- Code/analysis_functions/analyzeResults.m
+++ Code/analysis_functions/analyzeResults.m
@@ -115,66 +115,69 @@ h5write(analysisFile,'/Analysis/Znormf_real',real(Znormf));
 h5create(analysisFile,'/Analysis/Znormf_imag',size(Znormf));
 h5write(analysisFile,'/Analysis/Znormf_imag',imag(Znormf));
 
+%% Step 7: compute the distributions
+computeDistributions(Znormf,alpha, nBins,nRCM, foldername, handles)
+
 %% Step 7: Generate a distribution of the normalized Z parameters from Step 6
-[Zhist_EXP,Zbin_EXP,Zphist_EXP,Zpbin_EXP] = computeMeasuredDistribution(Znormf,foldername,handles);
-
-h5create(analysisFile,'/Analysis/Zhist_EXP',size(Zhist_EXP));
-h5write(analysisFile,'/Analysis/Zhist_EXP',Zhist_EXP);
-h5create(analysisFile,'/Analysis/Zbin_EXP',size(Zbin_EXP));
-h5write(analysisFile,'/Analysis/Zbin_EXP',Zbin_EXP);
-h5create(analysisFile,'/Analysis/Zphist_EXP',size(Zphist_EXP));
-h5write(analysisFile,'/Analysis/Zphist_EXP',Zphist_EXP);
-h5create(analysisFile,'/Analysis/Zpbin_EXP',size(Zpbin_EXP));
-h5write(analysisFile,'/Analysis/Zpbin_EXP',Zpbin_EXP);
-
-%% Step 8: Generate a distribution of Z parameters using random coupling model (RCM) using the loss parameter (alpha) computed in Step 4.
-[Zhist_RCM,Zbin_RCM, Zphist_RCM,Zpbin_RCM] = computeRCMDistribution(alpha,nRCM,foldername,handles);
-
-h5create(analysisFile,'/Analysis/Zhist_RCM',size(Zhist_RCM));
-h5write(analysisFile,'/Analysis/Zhist_RCM',Zhist_RCM);
-h5create(analysisFile,'/Analysis/Zbin_RCM',size(Zbin_RCM));
-h5write(analysisFile,'/Analysis/Zbin_RCM',Zbin_RCM);
-h5create(analysisFile,'/Analysis/Zphist_RCM',size(Zphist_RCM));
-h5write(analysisFile,'/Analysis/Zphist_RCM',Zphist_RCM);
-h5create(analysisFile,'/Analysis/Zpbin_RCM',size(Zpbin_RCM));
-h5write(analysisFile,'/Analysis/Zpbin_RCM',Zpbin_RCM);
+% [Zhist_EXP,Zbin_EXP,Zphist_EXP,Zpbin_EXP] = computeMeasuredDistribution(Znormf,foldername,handles);
+% 
+% h5create(analysisFile,'/Analysis/Zhist_EXP',size(Zhist_EXP));
+% h5write(analysisFile,'/Analysis/Zhist_EXP',Zhist_EXP);
+% h5create(analysisFile,'/Analysis/Zbin_EXP',size(Zbin_EXP));
+% h5write(analysisFile,'/Analysis/Zbin_EXP',Zbin_EXP);
+% h5create(analysisFile,'/Analysis/Zphist_EXP',size(Zphist_EXP));
+% h5write(analysisFile,'/Analysis/Zphist_EXP',Zphist_EXP);
+% h5create(analysisFile,'/Analysis/Zpbin_EXP',size(Zpbin_EXP));
+% h5write(analysisFile,'/Analysis/Zpbin_EXP',Zpbin_EXP);
+% 
+% %% Step 8: Generate a distribution of Z parameters using random coupling model (RCM) using the loss parameter (alpha) computed in Step 4.
+% [Zhist_RCM,Zbin_RCM, Zphist_RCM,Zpbin_RCM] = computeRCMDistribution(alpha,nRCM,foldername,handles);
+% 
+% h5create(analysisFile,'/Analysis/Zhist_RCM',size(Zhist_RCM));
+% h5write(analysisFile,'/Analysis/Zhist_RCM',Zhist_RCM);
+% h5create(analysisFile,'/Analysis/Zbin_RCM',size(Zbin_RCM));
+% h5write(analysisFile,'/Analysis/Zbin_RCM',Zbin_RCM);
+% h5create(analysisFile,'/Analysis/Zphist_RCM',size(Zphist_RCM));
+% h5write(analysisFile,'/Analysis/Zphist_RCM',Zphist_RCM);
+% h5create(analysisFile,'/Analysis/Zpbin_RCM',size(Zpbin_RCM));
+% h5write(analysisFile,'/Analysis/Zpbin_RCM',Zpbin_RCM);
 
 %% Step 9: Plot the pmf
-hh1 = figure('Position',[10 100 800 800],'NumberTitle', 'off', 'Name', 'RCM Magnitude Comparison'); 
-hold off;
-paramlables = cellstr(['11';'12';'21';'22']);
-for i = 1:num_ports^2
-    p= 10;         
-    subplot(num_ports,num_ports,i);
-    Zpmf_RCM(:,i) = Zhist_RCM(:,i)./((Zbin_RCM(2,i)-Zbin_RCM(1,i))*sum(Zhist_RCM(:,i))); % Create a pmf from histogram for RCM prediction of the normalized impedance
-    Zpmf_EXP(:,i) = Zhist_EXP(:,i)./((Zbin_EXP(2,i)-Zbin_EXP(1,i))*sum(Zhist_EXP(:,i))); % Create a pmf from histogram for measurement of the normalized impedance
-    plot(Zbin_RCM(:,i),Zpmf_RCM(:,i),'o-g','MarkerSize',2); hold on;             %Plot the pmf (divide by the area under curve) of the RCM predicted normalized impedance
-    plot(Zbin_EXP(:,i),Zpmf_EXP(:,i),'*-k','MarkerSize',5);       %Plot the pmf (divide by the area under curve) of the measured normalized impedance
-    %              set(gca, 'XTickLabel', '', 'YTickLabel', '');
-    legend('RCM Prediction','Measurement')
-    xlabel(strcat('Normalized |Z_{',char(paramlables{i}),'}|')); ylabel('Probability Density Function'); title(['PDF of |Z_{',char(paramlables{i}),'}^{norm}| for Cylindrical Chamber with ',...
-        '\alpha = ',num2str(round(alpha(i)))]);
-    axis tight
-end
-hh2 = figure('Position',[10 100 800 800],'NumberTitle', 'off', 'Name', 'RCM Phase Comparison'); 
-hold off; clear Zppmf_RCM; clear Zppmf_EXP;
-for i = 1:num_ports^2
-    p= 10;         
-    subplot(num_ports,num_ports,i);
-    Zppmf_RCM(:,i) = Zphist_RCM(:,i)./((Zpbin_RCM(2,i)-Zpbin_RCM(1,i))*sum(Zphist_RCM(:,i))); % Create a pmf from histogram for RCM prediction of the normalized impedance
-    Zppmf_EXP(:,i) = Zphist_EXP(:,i)./((Zpbin_EXP(2,i)-Zpbin_EXP(1,i))*sum(Zphist_EXP(:,i))); % Create a pmf from histogram for measurement of the normalized impedance
-    plot(Zpbin_RCM(:,i),Zppmf_RCM(:,i),'o-g','MarkerSize',2); hold on;             %Plot the pmf (divide by the area under curve) of the RCM predicted normalized impedance
-    plot(Zpbin_EXP(:,i),Zppmf_EXP(:,i),'*-k','MarkerSize',5);       %Plot the pmf (divide by the area under curve) of the measured normalized impedance
-    %              set(gca, 'XTickLabel', '', 'YTickLabel', '');
-    legend('RCM Prediction','Measurement')  
-    xlabel(strcat('Normalized |Z_{',char(paramlables{i}),'}|')); ylabel('Probability Density Function'); title(['PDF of phase(Z_{',char(paramlables{i}),'}^{norm}) for Cylindrical Chamber with ',...
-        '\alpha = ',num2str(round(alpha(i)))]);
-    axis tight
-end
-
-
-saveas(hh1,fullfile(foldername,'RCM_mag_comparisons'),'png');
-saveas(hh2,fullfile(foldername,'RCM_phase_comparisons'),'png');
-close(hh1)
-close(hh2)
+% hh1 = figure('Position',[10 100 800 800],'NumberTitle', 'off', 'Name', 'RCM Magnitude Comparison'); 
+% hold off;
+% paramlables = cellstr(['11';'12';'21';'22']);
+% for i = 1:num_ports^2
+%     p= 10;         
+%     subplot(num_ports,num_ports,i);
+%     Zpmf_RCM(:,i) = Zhist_RCM(:,i)./((Zbin_RCM(2,i)-Zbin_RCM(1,i))*sum(Zhist_RCM(:,i))); % Create a pmf from histogram for RCM prediction of the normalized impedance
+%     Zpmf_EXP(:,i) = Zhist_EXP(:,i)./((Zbin_EXP(2,i)-Zbin_EXP(1,i))*sum(Zhist_EXP(:,i))); % Create a pmf from histogram for measurement of the normalized impedance
+%     plot(Zbin_RCM(:,i),Zpmf_RCM(:,i),'o-g','MarkerSize',2); hold on;             %Plot the pmf (divide by the area under curve) of the RCM predicted normalized impedance
+%     plot(Zbin_EXP(:,i),Zpmf_EXP(:,i),'*-k','MarkerSize',5);       %Plot the pmf (divide by the area under curve) of the measured normalized impedance
+%     %              set(gca, 'XTickLabel', '', 'YTickLabel', '');
+%     legend('RCM Prediction','Measurement')
+%     xlabel(strcat('Normalized |Z_{',char(paramlables{i}),'}|')); ylabel('Probability Density Function'); title(['PDF of |Z_{',char(paramlables{i}),'}^{norm}| for Cylindrical Chamber with ',...
+%         '\alpha = ',num2str(round(alpha(i)))]);
+%     axis tight
+% end
+% hh2 = figure('Position',[10 100 800 800],'NumberTitle', 'off', 'Name', 'RCM Phase Comparison'); 
+% hold off; clear Zppmf_RCM; clear Zppmf_EXP;
+% for i = 1:num_ports^2
+%     p= 10;         
+%     subplot(num_ports,num_ports,i);
+%     Zppmf_RCM(:,i) = Zphist_RCM(:,i)./((Zpbin_RCM(2,i)-Zpbin_RCM(1,i))*sum(Zphist_RCM(:,i))); % Create a pmf from histogram for RCM prediction of the normalized impedance
+%     Zppmf_EXP(:,i) = Zphist_EXP(:,i)./((Zpbin_EXP(2,i)-Zpbin_EXP(1,i))*sum(Zphist_EXP(:,i))); % Create a pmf from histogram for measurement of the normalized impedance
+%     plot(Zpbin_RCM(:,i),Zppmf_RCM(:,i),'o-g','MarkerSize',2); hold on;             %Plot the pmf (divide by the area under curve) of the RCM predicted normalized impedance
+%     plot(Zpbin_EXP(:,i),Zppmf_EXP(:,i),'*-k','MarkerSize',5);       %Plot the pmf (divide by the area under curve) of the measured normalized impedance
+%     %              set(gca, 'XTickLabel', '', 'YTickLabel', '');
+%     legend('RCM Prediction','Measurement')  
+%     xlabel(strcat('Normalized |Z_{',char(paramlables{i}),'}|')); ylabel('Probability Density Function'); title(['PDF of phase(Z_{',char(paramlables{i}),'}^{norm}) for Cylindrical Chamber with ',...
+%         '\alpha = ',num2str(round(alpha(i)))]);
+%     axis tight
+% end
+% 
+% 
+% saveas(hh1,fullfile(foldername,'RCM_mag_comparisons'),'png');
+% saveas(hh2,fullfile(foldername,'RCM_phase_comparisons'),'png');
+% close(hh1)
+% close(hh2)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ code/analysis_functions/computeDistributions.m
@@ -0,0 +1,72 @@
+function computeDistributions(Znormf,alpha, nBins,nRCM, foldername, varargin)
+
+if (nargin == 6)
+    useGUI = true;
+    handles = varargin{1};
+else
+    useGUI = false;
+end
+
+hMagFigure = figure('Position',[10 100 800 800],'NumberTitle', 'off', 'Name', 'Normalized Magnitude PMF from Measurement');
+hPhaseFigure = figure('Position',[10 100 800 800],'NumberTitle', 'off', 'Name', 'Normalized Phase PMF from Measurement');
+indstring = {'11','12','21','22'};
+
+for port = 1:4
+    
+    lstring = sprintf('Computing RCM distribution for port %d',port);
+    if (useGUI == true)
+        logMessage(handles.jEditbox,lstring);
+    else
+        disp(lstring)
+    end
+    
+    Zmeas = Znormf(:,port,:);
+    Zmeas = Zmeas(:);
+    ZRCM_2port =  genPMFrcm(alpha(port),2, nRCM);
+    Zrcm = ZRCM_2port(:,port);
+    
+    figure(hMagFigure);
+    subplot(2,2,port)
+    hzMagMeas(port) = histogram(abs(Zmeas),'normalization','pdf','DisplayStyle','stairs','LineWidth',2);
+    set(hzMagMeas(port),'NumBins',nBins);
+    hold on
+    hzMagRCM(port) = histogram(abs(Zrcm),'normalization','pdf','LineStyle','-.','DisplayStyle','stairs','LineWidth',2);
+    set(hzMagRCM(port),'NumBins',nBins);
+    grid on
+    set(gca,'LineWidth',2)
+    set(gca,'FontSize',12)
+    set(gca,'FontWeight','bold')
+    lstring = sprintf('|Z_{%s}|',indstring{port});
+    xlabel(lstring);
+    ylabel('PDF');
+    tstring = sprintf('PDF of |Z_{%s}|',indstring{port});
+    title(tstring);
+    legend('Measured','RCM');
+    
+    
+    figure(hPhaseFigure)
+    subplot(2,2,port)
+    hzPhaseMeas(port) = histogram(angle(Zmeas),'normalization','pdf','DisplayStyle','stairs','LineWidth',2);
+    set(hzPhaseMeas(port),'NumBins',nBins);
+    hold on
+    hzPhaseRCM(port) = histogram(angle(Zrcm),'normalization','pdf','LineStyle','-.','DisplayStyle','stairs','LineWidth',2);
+    set(hzPhaseRCM(port),'NumBins',nBins);
+    grid on
+    set(gca,'LineWidth',2)
+    set(gca,'FontSize',12)
+    set(gca,'FontWeight','bold')
+    lstring = sprintf('\\angleZ_{%s}',indstring{port});
+    xlabel(lstring);
+    ylabel('PDF');
+    tstring = sprintf('PDF of \\angleZ_{%s}',indstring{port});
+    title(tstring);
+    legend('Measured','RCM');
+
+end
+
+if (useGUI == true)
+    saveas(hMagFigure,fullfile(foldername,'PDF_Comparison_Mag'),'png');
+    saveas(hh2,fullfile(foldername,'PDF_Comparison_Phase'),'png');
+    close(hPhaseFigure)
+    close(hh2)
+end
