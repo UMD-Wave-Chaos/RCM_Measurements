394846fd1cc722d49b6a29a84a25aed76afb6799
Benjamin Frazier <benjaminfrazier@benjamins-imac.lan>
2018-10-28 23:22:21 -0400

    updating PDP function

--- Code/analysis_functions/computePowerDecayProfile.m
+++ Code/analysis_functions/computePowerDecayProfile.m
@@ -1,96 +1,92 @@
+function [tau,pdp] = computePowerDecayProfile(SCt,t,l,index,foldername,varargin)
 
-
-pdp = mean(abs(SCt(:,3,:)).^2,3);
-
-nPoints = 100;
-stepSize = 25;
-done = 0;
-
-previousPDPSum = 0;
-currentIndex = 1;
-increasing = false;
-decreasing = false;
-
-index = 1;
-
-val = [];
-
-% while done ~= 3
-%     
-%     if (currentIndex + nPoints > length(pdp))
-%         done = 3;
-%         break;
-%     end
-%     
-%     pdpSum =  sum(pdp(currentIndex:currentIndex + nPoints));
-%     
-%     if pdpSum > previousPDPSum
-%         increasing = true;
-%         decreasing = false;
-%     elseif pdpSum < previousPDPSum && t(currentIndex) > 0
-%         increasing = false;
-%         decreasing = true;
-% %         done = done + 1;
-%         index = currentIndex + floor(nPoints/2);
-%     end
-%     val(currentIndex) = pdpSum;
-%     previousPDPSum = pdpSum;
-%     currentIndex = currentIndex + 1;
-%        
-% end
-
-smoothPDP = af*smooth(pdp,sf);
-
-testVal = 0;
-for cnt = 1:length(smoothPDP) - nPoints
-    
-    pdpSum = sum(smoothPDP(cnt:cnt + nPoints));
-    
-    if pdpSum < previousPDPSum && t(cnt) > 0
-        testVal = testVal + 1;
-    end
-    
-    previousPDPSum = pdpSum;
-    
-    if (testVal > 3)
-        index = cnt + floor(nPoints/2);
-        break;
-    end
+if nargin == 6
+    savePlots = varargin{1};
+else
+    savePlots = 0;
 end
 
-tBreakLate = t(index) + 0.5e-6;
-tBreakEarly = t(index)/2;
+%compute the power decay profile
+%PDP = <| IFT{SC}|^2>
+pdp = mean(abs(SCt(:,index,:)).^2,3);
 
+%set the break points in time for the number of electrical lengths
+%try 500 to 1500
+tStart = l*500/3E8;
+tStop = l*1500/3E8;
+ 
+%convert from time to indices
+indStart = find(abs(t- tStart) == min(abs(t-tStart)));              
+indStop = find(abs(t - tStop) == min(abs(t - tStop)));             
 
-earlyStart = find(abs(t) == min(abs(t)));
-earlyStop = find(abs(t- tBreakEarly) == min(abs(t-tBreakEarly)));  
-lateStart = find(abs(t- tBreakLate) == min(abs(t-tBreakLate)));              
-lateStop = find(abs(t-((l*1500)/3E8)) == min(abs(t-((l*1500)/3E8))));             
-
-pdpEarly = pdp(earlyStart:earlyStop);
-timeEarly = t(earlyStart:earlyStop);
-pdpLate = pdp(lateStart:lateStop);
-timeLate = t(lateStart:lateStop);
+%get the sections
+pdpSection = pdp(indStart:indStop);
+timeSection = t(indStart:indStop);
 
 %get the "early time" and the "late time"
 af = 10000;
 sf = 1000;
-smoothPDPEarly = af*smooth(pdpEarly,sf);
-smoothPDPLate = af*smooth(pdpLate,sf);
 
+%smooth the data
+smoothPDPSection= af*smooth(pdpSection,sf);
 
-figure;
-plot(t*1e6,pdp);
+%fit the data to an exponential term
+gFit= fit(timeSection,smoothPDPSection, fittype('exp1'));
+
+%get the time constant of the cavity
+tau = -1/gFit.b;
+
+%plot the results
+%get the index string
+if index == 1
+    indString = '11';
+elseif index == 2
+    indString = '12';
+elseif index == 3
+    indString = '21';
+else
+    indString = '22';
+end
+
+tString = sprintf('S%s PDP',indString);
+h1 = figure('Name', tString);
+subplot(2,1,1)
+plot(timeSection*1e6,pdpSection);
 hold on
-plot(timeEarly*1e6,smoothPDPEarly/af,'LineWidth',2);
-plot(timeLate*1e6,smoothPDPLate/af,'LineWidth',2);
+plot(timeSection*1e6,smoothPDPSection/af,'LineWidth',2);
+plot(timeSection*1e6,gFit(timeSection)/af,'LineWidth',2);
+grid on
+xlabel('Time (\mus)')
+ylabel('PDP(t)')
+set(gca,'LineWidth',2);
+set(gca,'FontSize',12);
+set(gca,'FontWeight','bold');
+legend('Raw','Smoothed','Fit');
+tString = sprintf('S_{%s} Linear PDP',indString);
+title(tString);
 
-gEarly = fit(timeEarly,smoothPDPEarly, fittype('exp2'));   
-gLate = fit(timeLate,smoothPDPLate, fittype('exp1'));   
+centerInd = indStart + floor((indStop-indStart)/2);
+tString = sprintf('\\tau = %0.3f ns',tau*1e9);
+text(t(centerInd)*1e6, gFit(t(centerInd))*2/af,tString,'FontWeight','bold','FontSize',12);
 
-tau_e = -1/gEarly.b;
-tau_rc = -1/gLate.b;
+subplot(2,1,2)
+plot(timeSection*1e6,20*log10(pdpSection));
+hold on
+plot(timeSection*1e6,20*log10(smoothPDPSection/af),'LineWidth',2);
+plot(timeSection*1e6,20*log10(gFit(timeSection)/af),'LineWidth',2);
+grid on
+xlabel('Time (\mus)')
+ylabel('PDP(t) (dB)')
+set(gca,'LineWidth',2);
+set(gca,'FontSize',12);
+set(gca,'FontWeight','bold');
+legend('Raw','Smoothed','Fit');
+tString = sprintf('S_{%s} Log Mag PDP',indString);
+title(tString);
 
-plot(timeEarly*1e6,gEarly(timeEarly)/af);
-plot(timeLate*1e6,gLate(timeLate)/af);
+if (savePlots)
+    fname = sprintf('S%s_PDP',indString);
+    saveas(hh1,fullfile(foldername,fname),'png');
+    close (hh1)
 
+end
