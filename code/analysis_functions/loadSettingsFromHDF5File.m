beb97b5771473fedaaa6b436b4c1aaa7dd7ffc72
Ben Frazier <bwfrazier@gmail.com>
2018-11-09 12:49:39 -0500

    fix for loading settings

--- code/analysis_functions/loadSettingsFromHDF5File.m
+++ code/analysis_functions/loadSettingsFromHDF5File.m
@@ -5,14 +5,6 @@ Settings.NOP = h5readatt(fileName,'/','Npoints');
 Settings.N = h5readatt(fileName,'/','Nrealizations');
 Settings.comPort = h5readatt(fileName,'/','ComPort');
 
-temp = h5readatt(fileName,'/','ECal');
-
-if (temp == 1)
-    Settings.electronicCalibration = true;
-else
-    Settings.electronicCalibration = false;
-end
-
 Settings.Comments = strtrim(h5readatt(fileName,'/','Comments'));
 Settings.V = h5readatt(fileName,'/','V');
 Settings.l = h5readatt(fileName,'/','l');
@@ -20,7 +12,5 @@ Settings.nRCM = h5readatt(fileName,'/','nRCM');
 
 Settings.fStart = h5readatt(fileName,'/','fStart');
 Settings.fStop = h5readatt(fileName,'/','fStop');
-Settings.transformStart = h5readatt(fileName,'/','transformStart');
-Settings.transformStop = h5readatt(fileName,'/','transformStop');
 Settings.nStepsPerRevolution = h5readatt(fileName,'/','nStepsPerRevolution');
 Settings.direction = h5readatt(fileName,'/','direction');
