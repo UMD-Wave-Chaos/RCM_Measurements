fb779dd73b0a4747009b9aadbac4c4f2d3a08afb
Benjamin Frazier <benjaminfrazier@benjamins-imac.lan>
2018-12-15 01:12:55 -0500

    adding matlab script

--- code/Matlab/analysis_functions/getComplexFromHDF5.m
+++ code/Matlab/analysis_functions/getComplexFromHDF5.m
@@ -4,7 +4,20 @@ function out = getComplexFromHDF5(filename,varname)
 rString = sprintf('%s_real',varname);
 iString = sprintf('%s_imag',varname);
 
-realPart = h5read(filename,rString);
-imagPart = h5read(filename,iString);
+info = h5info(filename,rString);
+chunkSize = info.ChunkSize;
+totalSize = info.Dataspace.Size;
+
+nElements = totalSize/chunkSize;
+
+realPart = zeros(chunkSize,nElements);
+imagPart = zeros(chunkSize,nElements);
+
+for (cnt = 1:nElements)
+
+    realPart(:,cnt) = h5read(filename,rString,(cnt-1)*chunkSize + 1, chunkSize);
+    imagPart(:,cnt) = h5read(filename,iString,(cnt-1)*chunkSize + 1, chunkSize);
+
+end
 
 out = realPart + 1j*imagPart;
