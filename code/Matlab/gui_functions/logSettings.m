97f21d6586d1c58f2a2e71112dbbb42b78034595
Ben Frazier <bwfrazier@gmail.com>
2018-12-28 11:28:05 -0500

    updating gui code to log settings properly

--- code/Matlab/gui_functions/logSettings.m
+++ code/Matlab/gui_functions/logSettings.m
@@ -2,25 +2,19 @@ function logSettings(handles)
 
 logMessage(handles.jEditbox,'Configuration:','info')
 
-sString = sprintf('Number of Points: %d',handles.Settings.NOP);
+sString = sprintf('Number of Points: %d',handles.Settings.numberOfPoints);
 logMessage(handles.jEditbox,sString);
 
-sString = sprintf('Number of Realizations: %d',handles.Settings.N);
+sString = sprintf('Number of Realizations: %d',handles.Settings.numberOfRealizations);
 logMessage(handles.jEditbox,sString);
 
-sString = sprintf('COM Port: %s',handles.Settings.comPort);
+sString = sprintf('COM Port: %s',handles.Settings.COMPort);
 logMessage(handles.jEditbox,sString);
 
 sString = sprintf('File Name: %s',handles.Settings.fileName);
 logMessage(handles.jEditbox,sString);
 
-sString = sprintf('Antenna Electrical Length: %s',handles.Settings.l);
-logMessage(handles.jEditbox,sString);
-
-sString = sprintf('Cavity Volume: %s',handles.Settings.V);
-logMessage(handles.jEditbox,sString);
-
-sString = sprintf('Number of RCM Realizations: %d',handles.Settings.nRCM);
+sString = sprintf('Cavity Volume: %s',handles.Settings.cavityVolume);
 logMessage(handles.jEditbox,sString);
 
 sString = sprintf('Frequency Sweep Start: %0.2f GHz',handles.Settings.fStart*1e-9);
@@ -29,8 +23,5 @@ logMessage(handles.jEditbox,sString);
 sString = sprintf('Frequency Sweep Stop: %0.2f GHz',handles.Settings.fStop*1e-9);
 logMessage(handles.jEditbox,sString);
 
-sString = sprintf('Stepper Motor Number of Steps Per Revolution: %d',handles.Settings.nStepsPerRevolution);
-logMessage(handles.jEditbox,sString);
-
-sString = sprintf('Stepper Motor Direction: %d',handles.Settings.direction);
+sString = sprintf('Stepper Motor Number of Steps Per Revolution: %d',handles.Settings.numberOfStepsPerRevolution);
 logMessage(handles.jEditbox,sString);
